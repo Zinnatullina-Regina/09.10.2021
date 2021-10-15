67b835454a03289925e95176cf06dce5b0432ca5
Zinnatullina-Regina <zinnatullina.regina@gmail.com>
2021-10-15 23:02:46 +0300

    Добавьте файлы проекта.

---
+++ 09.10.2021/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+namespace _09._10._2021
+{
+    class Program
+    {
+        public class Worker
+        {
+            public static bool IsStupid(Worker worker)
+            {
+                return worker._isStupid;
+            }
+            public static ushort GetDegree(Worker worker)
+            {
+                return worker._degreeImpudence;
+            }
+            public Worker(string name, string direction, ushort degreeImpudence, bool isStupid)
+            {
+                _name = name;
+                _direction = direction;
+                _degreeImpudence = degreeImpudence;
+                _isStupid = isStupid;
+            }
+            private string _name;
+            private string _direction;
+            private ushort _degreeImpudence;
+            private bool _isStupid;
+        }
+        
+        static string CountFives (int[] FiveOne, int[] FiveTwo)
+        {
+            int countOne = 0, countTwo = 0;
+            for (int i = 0; i < FiveTwo.Length; i++)
+            {
+                FiveOne[i] = Convert.ToInt32(Console.ReadLine());
+                if (FiveOne[i] == 5)
+                {
+                    countOne++;
+                }
+            }
+
+            for (int i = 0; i < FiveOne.Length; i++)
+            {
+                FiveTwo[i] = Convert.ToInt32(Console.ReadLine());
+                if (FiveTwo[i] == 5)
+                {
+                    countTwo++;
+                }
+            }
+
+            if (countOne == countTwo)
+            {
+                return "Drinks All Round! Free Beers on Bjorg!";
+            }
+            else
+            {
+
+                return "Ой, Бьорг - пончик! Ни для кого пива!";
+
+            }
+
+
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("\nБухаловка");
+            int [] FiveOne = new int [10];
+            int[] FiveTwo = new int[10];
+            Console.WriteLine(CountFives(FiveOne,FiveTwo));
+
+
+
+
+
+            Console.WriteLine("\nЗадание 3");
+            var People = new Dictionary<string, (int, int)>
+            {
+                ["Tixonov Alex"] = ( 2004, 250),
+                ["Zinnatullina Regina"] = ( 2003, 260),
+                ["Senkyм Lila"] = (2003, 267),
+                ["Ivanoм Ivan"] = ( 2002, 257),
+                ["Kipikov Pavel"] = ( 2004, 269),
+                ["Trockiy Dima"] = ( 2002, 270),
+                ["Remil Anila"] = ( 2003, 240),
+                ["Artix Fox"] = ( 2003, 250),
+                ["Atim Alia"] = ( 2003, 240)
+            };
+            
+            bool contin = true;
+            string vvod;
+
+            while (contin) {
+                vvod = Console.ReadLine();
+
+         
+                if (vvod == "Новый студент")
+                {
+                    Console.WriteLine("\nВведите фамилию и имя");
+                    string sn = Console.ReadLine();
+                    Console.WriteLine("\nВведите год рождения");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\nВведите кол-во баллов");
+                    int points = Convert.ToInt32(Console.ReadLine());
+                    People.Add(sn, (y,points));
+                    Console.WriteLine(People);
+                }
+                if (vvod == "Удалить")
+                {
+
+                    string Sn = Console.ReadLine();
+                    People.Remove(Sn);
+
+                    Console.WriteLine(People);
+
+                }
+
+                if (vvod == "Сортировать")
+                {
+
+                    People = People.OrderBy(x => x.Value.Item2).ToDictionary(x => x.Key,x => x.Value) ;
+
+                    Console.WriteLine(People);
+                }
+                if (vvod == "Закрыть") {
+
+                    contin = false;
+                }
+
+
+            }
+
+
+
+            Console.WriteLine("4");
+            Console.WriteLine("Сколько работников?");
+            int countWorkers;
+            while (!int.TryParse(Console.ReadLine(), out countWorkers) || countWorkers < 0)
+            {
+                Console.Write("Неверный ввод,повторите! ");
+            }
+            int countTables = countWorkers / 2;
+            int capacityTables = 4;
+            Console.Clear();
+            var listWorkers = new List<Worker>();
+            for (int i = 0; i < countWorkers; i++)
+            {
+                Console.Write("Введите имя работника - ");
+                string name = Console.ReadLine();
+                Console.Write("Где он работает? - ");
+                string direction = Console.ReadLine();
+                Console.Write("Введите его степень наглости(только целое значение) - ");
+                ushort degreeImpudence;
+                while (!ushort.TryParse(Console.ReadLine(), out degreeImpudence) || degreeImpudence < 0)
+                {
+                    Console.WriteLine("Неверный ввод!");
+                }
+                bool isStupid;
+                Console.WriteLine("Введите тупой ли он? да / нет");
+                if (Console.ReadLine().ToLower().Equals("да"))
+                {
+                    isStupid = true;
+                }
+                else
+                {
+                    isStupid = false;
+                }
+                listWorkers.Add(new Worker(name, direction, degreeImpudence, isStupid));
+            }
+
+            
+            for (int i = 0; i < listWorkers.Count; i++)
+            {
+                for (int j = 0; j < listWorkers.Count - 1; j++)
+                {
+                    if (Worker.GetDegree(listWorkers[j]) > Worker.GetDegree(listWorkers[j + 1]) && Worker.IsStupid(listWorkers[j]))
+                    {
+                        var temp = listWorkers[j + Worker.GetDegree(listWorkers[j])];
+                        listWorkers[j + Worker.GetDegree(listWorkers[j])] = listWorkers[j];
+                        listWorkers[j] = temp;
+                    }
+                }
+
+            }
+            var queue = new Queue<Worker>();
+            for (int i = 0; i < listWorkers.Count; i++)
+            {
+                queue.Enqueue(listWorkers[i]);
+            }
+            var listTables = new List<Stack<Worker>>();
+            for (int i = 0; i < countTables; i++)
+            {
+                listTables.Add(new Stack<Worker>());
+                for (int j = 0; j < capacityTables; j++)
+                {
+                    listTables[i].Push(queue.Peek());
+                }
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 6/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static int GetCount(char[] array, char[] ar)
+        {
+            int Count = 0;
+
+            foreach (char ch in array)
+            {
+                foreach (char cha in ar)
+                {
+                    if (ch == cha)
+                    {
+                        Count++;
+                    }
+                }
+            }
+            return Count;
+        }
+
+        static int GetCount1( List <char> array,  List <char> ar)
+        {
+            int Count = 0;
+
+            foreach (char ch in array)
+            {
+                foreach (char cha in ar)
+                {
+                    if (ch == cha)
+                    {
+                        Count++;
+                    }
+                }
+            }
+            return Count;
+        }
+
+
+        public static double PrintMeanValueForEachMonth(int sum)
+        {
+
+            return sum / 30;
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Задание 1");
+            char[] vowels = "АЕЁИОУЫЭЮЯ".ToLower().ToCharArray(); 
+            char[] consonants = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩЬЪ".ToLower().ToCharArray(); 
+
+            string str = File.ReadAllText(@"09.10.2021.txt");
+            char[] array = str.ToLower().ToCharArray(); 
+
+            int vowelsCount = GetCount(array, vowels);
+            int consonantsCount = GetCount(array, consonants); 
+
+            Console.WriteLine("Гласных в массиве: " + vowelsCount);
+            Console.WriteLine("Согласных в массиве: " + consonantsCount);
+
+
+
+            Console.WriteLine("\nМесяцы");
+            Random rnd = new Random();
+            int[,] tem = new int[12,30];
+            
+            for (int i = 0; i < tem.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < tem.GetLength(1); j++)
+                {
+                    tem[i, j] = rnd.Next(-30, 30);
+                    Console.Write(tem[i, j] + " ");
+                    sum += tem[i, j];
+                    if (j == tem.GetLength(1) - 1)
+                    {
+                        Console.WriteLine("\nСреднее");
+
+                        Console.WriteLine(PrintMeanValueForEachMonth(sum));
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n List");
+            List<char> Vowels = new List<char>("АЕЁИОУЫЭЮЯ".ToLower().ToCharArray()); 
+            List<char> Consonants = new List<char>("БВГДЖЗЙКЛМНПРСТФХЦЧШЩЬЪ".ToLower().ToCharArray()); 
+            FileStream Stream = new FileStream(@"09.10.2021.txt", FileMode.Open);
+            StreamReader Reader = new StreamReader(Stream);
+            string stR1 = Reader.ReadToEnd();
+            Stream.Close();
+
+            List<char> Str1 = new List<char>(stR1.ToLower().ToCharArray()); 
+
+            int VowelsCount = GetCount1(Str1, Vowels); 
+            int ConsonantsCount = GetCount1(Str1, Consonants); 
+
+            Console.WriteLine("Гласных в массиве: " + VowelsCount);
+            Console.WriteLine("Согласных в массиве: " + ConsonantsCount);
+
+
+
+            Console.WriteLine("\nПроверка пароля - двоичный в строку");
+            string s = File.ReadAllText(@"password.txt");
+            string result = new string(s.Split(new[] { ' ' }).Select(i => (char)(Convert.ToInt32(i, 2))).ToArray());
+            Console.WriteLine("\nЗадание 3");
+            int col = Convert.ToInt32(Console.ReadLine());
+            string[] passvords = new string[col];
+            bool temp = true;
+            for (int i = 0; i < passvords.Length; i++)
+            {
+                passvords[i] = Console.ReadLine();
+                if (passvords[i] == result)
+                {
+                    Console.WriteLine(result);
+                    temp = false;
+                }
+
+            }
+            if (temp)
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.WriteLine("\nАдская кухня");
+            string stR = Console.ReadLine();
+            string[] words = stR.Split(new char[] { ' ' });
+            string word1 = words[0];
+            string word2 = words[1];
+            string word3 = words[2];
+            string word4 = words[3];
+            string alfavit = "ЕЁИОУЫЭЮЯ";
+            for (int i = 0; i < 4; i++)
+            {
+
+                words[i] = words[i].ToUpper();
+                words[i] = words[i] + "!!!!";
+                words[i] = words[i].Replace("А", "@");
+                foreach (var k in alfavit)
+                {
+                    words[i] = words[i].Replace(k, '*');
+                }
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(words[i]);
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
